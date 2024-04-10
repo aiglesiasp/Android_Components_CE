1abc4c6f566c7dab4b7d76ecd3f034a0ed14f549
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-04-11 00:46:17 +0200

    Android Manifest with Internet permission and gradle with COIL implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // COIL - Para imagenes
+    implementation(libs.coil.compose)
 }
