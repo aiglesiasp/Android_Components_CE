2442df2180e8e70efc46fd75e784923ec7762379
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-04-11 02:23:22 +0200

    MyAppTheme Composable

--- app/src/main/java/com/aiglepub/composecomponents/MainActivity.kt
+++ app/src/main/java/com/aiglepub/composecomponents/MainActivity.kt
@@ -3,33 +3,22 @@ package com.aiglepub.composecomponents
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.aiglepub.composecomponents.examples.estados.MyStateSample
-import com.aiglepub.composecomponents.examples.estados.MyStateSampleFather
-import com.aiglepub.composecomponents.ui.medialist.MediaList
-import com.aiglepub.composecomponents.ui.theme.ComposeComponentsTheme
+import com.aiglepub.composecomponents.ui.MyComposeComponentsApp
+import com.aiglepub.composecomponents.ui.screens.main.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeComponentsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MyStateSampleFather()
-                }
-            }
+            MyComposeComponentsApp(content = { MainScreen() })
         }
     }
 }
+
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
