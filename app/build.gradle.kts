9b26850295b1205ac40b51a119c411cb82ecf218
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-04-11 02:21:28 +0200

    Agregada libreria Icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation (libs.androidx.material.icons.extended)
+
 
     // COIL - Para imagenes
     implementation(libs.coil.compose)
